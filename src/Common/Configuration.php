98b14b38f989fc7fa706e2d60d16e5180e4bb20e
zhangpeihua <zhangpeihua@bytedance.com>
2023-06-07 11:59:43 +0800

    feat: first init

---
+++ src/Common/Configuration.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace Volcengine\Common;
+
+class Configuration
+{
+    private static $defaultConfiguration;
+
+    protected $region = '';
+
+    protected $ak = '';
+
+    protected $sk = '';
+
+    protected $host = 'open.volcengineapi.com';
+
+    protected $userAgent = 'volcstack-php-sdk/1.0.0';
+
+    /**
+     * Debug switch (default set to false)
+     *
+     * @var bool
+     */
+    protected $debug = false;
+
+    /**
+     * Debug file location (log to STDOUT by default)
+     *
+     * @var string
+     */
+    protected $debugFile = 'php://output';
+
+    /**
+     * Debug file location (log to STDOUT by default)
+     *
+     * @var string
+     */
+    protected $tempFolderPath;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->tempFolderPath = sys_get_temp_dir();
+    }
+
+    /**
+     * Sets the host
+     *
+     * @param string $host Host
+     *
+     * @return $this
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+        return $this;
+    }
+
+    /**
+     * Gets the host
+     *
+     * @return string Host
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param string $ak
+     */
+    public function setAk($ak)
+    {
+        $this->ak = $ak;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAk()
+    {
+        return $this->ak;
+    }
+
+    /**
+     * @param string $sk
+     */
+    public function setSk($sk)
+    {
+        $this->sk = $sk;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSk()
+    {
+        return $this->sk;
+    }
+
+    /**
+     * @param string $region
+     */
+    public function setRegion($region)
+    {
+        $this->region = $region;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRegion()
+    {
+        return $this->region;
+    }
+
+    /**
+     * Gets the user agent of the api client
+     *
+     * @return string user agent
+     */
+    public function getUserAgent()
+    {
+        return $this->userAgent;
+    }
+
+    /**
+     * Sets debug flag
+     *
+     * @param bool $debug Debug flag
+     *
+     * @return $this
+     */
+    public function setDebug($debug)
+    {
+        $this->debug = $debug;
+        return $this;
+    }
+
+    /**
+     * Gets the debug flag
+     *
+     * @return bool
+     */
+    public function getDebug()
+    {
+        return $this->debug;
+    }
+
+    /**
+     * Sets the debug file
+     *
+     * @param string $debugFile Debug file
+     *
+     * @return $this
+     */
+    public function setDebugFile($debugFile)
+    {
+        $this->debugFile = $debugFile;
+        return $this;
+    }
+
+    /**
+     * Gets the debug file
+     *
+     * @return string
+     */
+    public function getDebugFile()
+    {
+        return $this->debugFile;
+    }
+
+    /**
+     * Sets the temp folder path
+     *
+     * @param string $tempFolderPath Temp folder path
+     *
+     * @return $this
+     */
+    public function setTempFolderPath($tempFolderPath)
+    {
+        $this->tempFolderPath = $tempFolderPath;
+        return $this;
+    }
+
+    /**
+     * Gets the temp folder path
+     *
+     * @return string Temp folder path
+     */
+    public function getTempFolderPath()
+    {
+        return $this->tempFolderPath;
+    }
+
+    /**
+     * Gets the default configuration instance
+     *
+     * @return Configuration
+     */
+    public static function getDefaultConfiguration()
+    {
+        if (self::$defaultConfiguration === null) {
+            self::$defaultConfiguration = new Configuration();
+        }
+
+        return self::$defaultConfiguration;
+    }
+
+    /**
+     * Sets the detault configuration instance
+     *
+     * @param Configuration $config An instance of the Configuration Object
+     *
+     * @return void
+     */
+    public static function setDefaultConfiguration(Configuration $config)
+    {
+        self::$defaultConfiguration = $config;
+    }
+
+    /**
+     * Gets the essential information for debugging
+     *
+     * @return string The report for debugging
+     */
+    public static function toDebugReport()
+    {
+        $report = 'PHP SDK (Volcengine\Common) Debug Report:' . PHP_EOL;
+        $report .= '    OS: ' . php_uname() . PHP_EOL;
+        $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
+        $report .= '    OpenAPI Spec Version: common-version' . PHP_EOL;
+        $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
+
+        return $report;
+    }
+}
---
+++ src/Common/Utils.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Volcengine\Common;
+
+class Utils
+{
+    public static function transRequest($data, $prefix = "")
+    {
+        $result = array();
+
+        foreach ($data::swaggerTypes() as $property => $swaggerType) {
+            $getter = $data::getters()[$property];
+            $value = $data->$getter();
+
+            if ($value !== null) {
+                if (is_array($value)) {
+                    foreach ($value as $key => $element) {
+                        if (is_object($element)) {
+                            $back = self::transRequest($element, $prefix . $data::attributeMap()[$property] . "." . ($key + 1) . ".");
+                            $result = array_merge($result, $back);
+                        } else {
+                            $result[$prefix . $data::attributeMap()[$property] . "." . ($key + 1)] = $element;
+                        }
+                    }
+                } elseif (is_object($value)) {
+                    $back = self::transRequest($value, $prefix . $data::attributeMap()[$property] . ".");
+                    $result = array_merge($result, $back);
+                } else {
+                    $result[$prefix . $data::attributeMap()[$property]] = $value;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    public static function signv4($ak, $sk, $region, $service, $body, $query, $method, $path, $headers)
+    {
+        if ($path === '') {
+            $path = '/';
+        }
+
+        $ldt = gmdate('Ymd\THis\Z');
+        $sdt = substr($ldt, 0, 8);
+        $headers['X-Date'] = $ldt;
+
+        $bodyHash = hash('sha256', $body);
+        $headers['X-Content-Sha256'] = $bodyHash;
+
+        $credentialScope = "$sdt/$region/$service/request";
+
+        $signedHeaders = [];
+        foreach ($headers as $key => $value) {
+            if ($key == "Host" || $key == "Content-Md5" || $key == "Content-Type" || substr($key, 0, 2) == "X-") {
+                $key = strtolower($key);
+                $signedHeaders[$key] = $value;
+            }
+        }
+        ksort($signedHeaders);
+
+        $signed_str = '';
+        foreach ($signedHeaders as $k => $v) {
+            $signed_str .= $k . ':' . $v . "\n";
+        }
+
+        $signedHeadersString = implode(';', array_keys($signedHeaders));
+        $canon = implode("\n", array($method, $path, $query, $signed_str, $signedHeadersString, $bodyHash));
+        $hash = hash('sha256', $canon);
+        $toSign = implode("\n", array("HMAC-SHA256", $ldt, $credentialScope, $hash));
+        $signingKey = self::getSigningKey($sdt, $region, $service, $sk);
+        $signature = hash_hmac('sha256', $toSign, $signingKey);
+        $credential = $ak . '/' . $credentialScope;
+        $headers['Authorization'] = "HMAC-SHA256 Credential={$credential}, SignedHeaders={$signedHeadersString}, Signature={$signature}";
+        return $headers;
+    }
+
+    public static function getSigningKey($date, $region, $service, $sk)
+    {
+        $dateKey = hash_hmac('sha256', $date, $sk, true);
+        $regionKey = hash_hmac('sha256', $region, $dateKey, true);
+        $serviceKey = hash_hmac('sha256', $service, $regionKey, true);
+        return hash_hmac('sha256', 'request', $serviceKey, true);
+    }
+}
