c3c0762454154e523fd436549142f451c504353b
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2024-11-21 11:12:29 +0800

    feat: auto generate for cv-PHP-2024-06-06-online-795-2024_11_21_11_12_00

--- src/Common/Configuration.php
+++ src/Common/Configuration.php
@@ -14,7 +14,7 @@ class Configuration
 
     protected $host = 'open.volcengineapi.com';
 
-    protected $userAgent = 'volcstack-php-sdk/1.0.34';
+    protected $userAgent = 'volcstack-php-sdk/1.0.35';
 
     /**
      * Debug switch (default set to false)
