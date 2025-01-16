18746e823cc3804f89a8a7bef1fa14fef9268f47
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-01-16 12:13:24 +0800

    feat: auto generate for ark-PHP-2024-01-01-online-941-2025_01_16_12_12_44

--- src/Common/Configuration.php
+++ src/Common/Configuration.php
@@ -14,7 +14,7 @@ class Configuration
 
     protected $host = 'open.volcengineapi.com';
 
-    protected $userAgent = 'volcstack-php-sdk/1.0.41';
+    protected $userAgent = 'volcstack-php-sdk/1.0.42';
 
     /**
      * Debug switch (default set to false)
