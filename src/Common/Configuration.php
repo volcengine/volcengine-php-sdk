f7e339099d8e71c4cb6baa4fab82eb7c675b20c1
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-04-08 12:34:28 +0800

    feat: auto generate for tis-PHP-2024-07-31-online-1094-2025_04_08_12_33_58

--- src/Common/Configuration.php
+++ src/Common/Configuration.php
@@ -14,7 +14,7 @@ class Configuration
 
     protected $host = 'open.volcengineapi.com';
 
-    protected $userAgent = 'volcstack-php-sdk/1.0.51';
+    protected $userAgent = 'volcstack-php-sdk/1.0.52';
 
     /**
      * Debug switch (default set to false)
