fa43dead03bdc4a5abd6a64991fa04d8f62b05d0
xiujie.1 <xiujie.1@bytedance.com>
2025-04-17 19:02:52 +0800

    update version and commitId

--- src/Common/Configuration.php
+++ src/Common/Configuration.php
@@ -14,7 +14,7 @@ class Configuration
 
     protected $host = 'open.volcengineapi.com';
 
-    protected $userAgent = 'volcstack-php-sdk/1.0.52';
+    protected $userAgent = 'volcstack-php-sdk/1.0.54';
 
     /**
      * Debug switch (default set to false)
