2faa83a554fddc45699a9308c0abd5478f806865
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-09-25 16:20:57 +0800

    feat: auto generate for directconnect-PHP-2020-04-01-online-1642-2025_09_25_16_20_17

--- src/Common/Configuration.php
+++ src/Common/Configuration.php
@@ -14,7 +14,7 @@ class Configuration
 
     protected $host = 'open.volcengineapi.com';
 
-    protected $userAgent = 'volcstack-php-sdk/1.0.76';
+    protected $userAgent = 'volcstack-php-sdk/1.0.77';
 
     /**
      * Debug switch (default set to false)
