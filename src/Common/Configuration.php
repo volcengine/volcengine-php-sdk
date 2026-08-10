ed746d6e3d49919fe913379b56de0df029a47acc
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2026-08-10 10:28:34 +0800

    feat: auto generate for tidb-PHP-2026-06-30-online-3046-2026_08_10_10_27_57

--- src/Common/Configuration.php
+++ src/Common/Configuration.php
@@ -36,7 +36,7 @@ class Configuration
     protected $httpProxy;
     protected $httpsProxy;
 
-    protected $userAgent = 'volcstack-php-sdk/1.0.128';
+    protected $userAgent = 'volcstack-php-sdk/1.0.129';
     protected $sdkUserAgent;
     protected $debug = false;
     protected $debugFile = 'php://output';
